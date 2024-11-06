a84e49346a8bdb34aac6a9e3da304fded2aee403
Alladin-51 <igortls20@gmail.com>
2024-11-06 18:12:48 -0300

    Paralax pronto + mecânica de morte

--- gamejamfga/Adult-bird.gd
+++ gamejamfga/Adult-bird.gd
@@ -3,18 +3,28 @@ extends CharacterBody2D
 # Variáveis para controlar a física do pulo e da queda
 @export var gravity = 700.0
 @export var jump_strength = -200.0
+var dead = false
 
 func _physics_process(delta):
 	# Aplica a gravidade ao personagem
 	velocity.y += gravity * delta
 	
 	# Detecta o pulo ao pressionar a tecla de ação "ui_accept" (geralmente a barra de espaço)
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and dead == false:
 		velocity.y = jump_strength  # Aplica a força de pulo
 		$AnimatedSprite2D.play("flapy")
-
+	
 	# Move o personagem no eixo Y
 	move_and_slide()
 	
+	
 func kill():
-	get_tree().reload_current_scene()
+	#get_tree().reload_current_scene()
+	var layer1 = get_parent().get_node("Layer1_Adult_Bird")
+	layer1.speed = 0
+	var layer2 = get_parent().get_node("layer_2_bird")
+	layer2.speed = 0
+	var layer3 = get_parent().get_node("layer_3_bird")
+	layer3.speed = 0
+	
+	dead = true
--- gamejamfga/Assets/Scenes/Levels/Bird/World/level_adult_bird.gd
+++ gamejamfga/Assets/Scenes/Levels/Bird/World/level_adult_bird.gd
@@ -19,6 +19,6 @@ func _on_timer_timeout():
 	pass # Replace with function body.
 	var enemy_instance = enemy_scene.instantiate()
 	self.add_child(enemy_instance)
-	if spawn_delay >0.4:
+	if spawn_delay >0.35:
 		spawn_delay -= 0.01
 	$Timer.start(spawn_delay)
