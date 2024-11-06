f340eb79f6a904bb1fd72b7f401b4653d7aecf16
Alladin-51 <igortls20@gmail.com>
2024-11-06 11:14:22 -0300

    Fase-pássaro

---
+++ gamejamfga/Adult-bird.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+# Variáveis para controlar a física do pulo e da queda
+@export var gravity = 700.0
+@export var jump_strength = -200.0
+
+func _physics_process(delta):
+	# Aplica a gravidade ao personagem
+	velocity.y += gravity * delta
+	
+	# Detecta o pulo ao pressionar a tecla de ação "ui_accept" (geralmente a barra de espaço)
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = jump_strength  # Aplica a força de pulo
+
+	# Move o personagem no eixo Y
+	move_and_slide()
---
+++ gamejamfga/Assets/Scenes/Levels/Bird/Enemies/Enemie_bird.gd
@@ -0,0 +1,18 @@
+extends Area2D
+var speed = 75
+var min_position = 7
+var max_position = 173
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	randomize()
+	self.global_position.x = 326
+	self.global_position.y = randf_range(min_position, max_position)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#get_tree().reload_current_scene()
+	pass
+	self.global_position.x -= speed*delta
